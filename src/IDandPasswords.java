bba355258618899d68d6bd5f0afc3aad9e1357ab
Jahriq <jahriq704@gmail.com>
2023-11-11 07:07:35 -0500

    first commit

---
+++ src/IDandPasswords.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class IDandPasswords {
+
+    //storing two strings id and a password
+    HashMap<String,String> logininfo = new HashMap<String,String>();
+
+    //create a contructor for id and password
+    IDandPasswords(){
+        logininfo.put("Jason", "Apple");
+        logininfo.put("Tom", "tomato");
+        logininfo.put("Zack", "orange");
+    }
+    //create a getter method so any class can request it and use protected since it contains id/password
+
+
+    protected HashMap<String, String> getLogininfo() {
+        return logininfo;
+    }
+}
